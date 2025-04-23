5535f29cd7a21a8fdba17286f427d2da96dec0c5
Maria Letícia Oliveira Moneda <mona@Mac-mini-de-Maria.local>
2025-04-23 11:17:40 -0300

    feat:adding ex08

--- c01/ex08/ft_sort_int_tab.c
+++ c01/ex08/ft_sort_int_tab.c
@@ -11,6 +11,7 @@
 /* ************************************************************************** */
 
 #include <unistd.h>
+#include <stdio.h>
 
 void	ft_sort_int_tab(int *tab, int size);
 
@@ -18,11 +19,48 @@ void	ft_sort_int_tab(int *tab, int size)
 {
 	int	count;
 	int	count2;
-	int	x;
+	int	temporary;
+
+	count = 0;
+
+	while (count < size)
+	{
+		count2 = 0;
+		while (count2 < size)
+		{
+			if(tab[count] <= tab[count2])
+			{
+				temporary = tab[count];
+				tab[count] = tab[count2];
+				tab[count2] = temporary;
+			}
+			count2++;
+		}
+		count++;
+	}
+}
+
+int	main(void)
+{
+	int	tab[5] = {8, 7, 5, 3, 1};
+	int	size = 5;
+	int count;
+	int	count2;
 
 	count = 0;
 	count2 = 0;
-	x = 
-	size[count]
+
+	while (count < size)
+	{
+		printf("Array atual: %d\n ", tab[count]);
+		count++;
+	}
+
+	ft_sort_int_tab(tab, size);
+	while (count2 < size)
+	{
+		printf("Array depois da função: %d \n", tab[count2]);
+		count2++;
+	}
 }
 
