6e0aa744da63bac760a8a3ab40e164e85dfb03f0
Mona <maria-ol@student.42sp.org.br>
2025-05-08 19:08:35 -0300

    adding c05 finished

--- c05/ex05/ft_sqrt.c
+++ c05/ex05/ft_sqrt.c
@@ -6,31 +6,32 @@
 /*   By: maria-ol <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/07 22:26:39 by maria-ol          #+#    #+#             */
-/*   Updated: 2025/05/07 23:07:09 by maria-ol         ###   ########.fr       */
+/*   Updated: 2025/05/08 13:50:24 by maria-ol         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
 int	ft_sqrt(int nb)
 {
 	int	result;
 
 	result = 1;
-	if (nb <= 0) //4
+	if (nb <= 0)
 		return (0);
-	while (result * result <= nb) // 1 * 1 <= 4 ; 2 * 2 = 4
+	while (result * result <= nb)
 	{
-		if (result * result == nb) // 2 * 2 = 4 entao retorna 4
+		if (result * result == nb)
 			return (result);
 		result++;
 	}
 	return (0);
 }
-int	main(void)
-{
-	printf("Square Root of 0: %d\n", ft_sqrt(0));
-	printf("Square Root of -13: %d\n", ft_sqrt(-13));
-	printf("Square Root of 4: %d\n", ft_sqrt(4));
-	printf("Square Root of 69: %d\n", ft_sqrt(69));
-	printf("Square Root of 24: %d\n", ft_sqrt(24));
-}
+// #include <stdio.h>
+// int	main(void)
+// {
+// 	printf("Square Root of 12: %d\n", ft_sqrt(144));
+// 	printf("Square Root of -13: %d\n", ft_sqrt(-13));
+// 	printf("Square Root of 4: %d\n", ft_sqrt(4));
+// 	printf("Square Root of 36: %d\n", ft_sqrt(36));
+// 	printf("Square Root of 69: %d\n", ft_sqrt(69));
+// 	printf("Square Root of 25: %d\n", ft_sqrt(25));
+// }
